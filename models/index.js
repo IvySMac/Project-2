1851fae47a6607d32b78c51088661892b3f58e22
afrazier01 <amfrazier01@gmail.com>
2023-06-27 21:16:47 -0500

    Started relations

--- models/index.js
+++ models/index.js
@@ -4,6 +4,46 @@ const Threads = require('./Threads');
 const Messages  = require('./Messages');
 const Post = require('./Post')
 
+// User.hasMany(Message, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Project.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Threads, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+User.hasMany(Messages, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Messages.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Threads.belongsTo(User, {
+  foreignKey: 'user_id',
+}); 
+
+Post.hasMany(Product, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
 
 
 
