482380524a094b658f527153e44f5a4f98a3614a
Tiffany Swank <Tiff03nicole@gmail.com>
2023-06-28 10:48:19 -0500

    Got project running on npm start. Added basic handlebars.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const messagesRoutes = require('./messagesRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/message', messagesRoutes);
 
 module.exports = router;
