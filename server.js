172c374f2377d0df118205bbd94279d324e16ab5
Tiffany Swank <Tiff03nicole@gmail.com>
2023-06-28 10:01:45 -0500

    added handlerbars.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const session = require('express-session');
+const handlebars = require('handlebars');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
@@ -7,6 +9,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
 
 const sess = {
   secret: 'Super secret secret',
