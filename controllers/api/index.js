da971e4f2c6c0405fb54e2c13678c0d24f404bb5
unknown <barronpeterson56@gmail.com>
2023-07-06 18:23:48 -0500

    add post routes to the api controller index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const messagesRoutes = require('./messagesRoutes');
-const productsRoutes =  require('./productsRoutes')
+const productsRoutes =  require('./productsRoutes');
+const postRoutes = require('./postRoutes');
 
 router.use('/user', userRoutes);
 router.use('/message', messagesRoutes);
 router.use('/products', productsRoutes);
+router.use('/post',postRoutes);
 
 module.exports = router;
