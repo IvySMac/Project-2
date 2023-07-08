48ba59cb9cb4aeb8769fb6297ac2d7c507ecc4a2
Creature Feature <ivy.mccurdy@gmail.com>
2023-07-07 23:27:22 -0500

    began adding methods to thread route and worked on seed file

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 const User = require('./User');
 const Product = require('./Products');
-const Threads = require('./Threads');
+const Thread = require('./Threads');
 const Message  = require('./Messages');
 const Post = require('./post')
 
 
 
 
-module.exports = { User, Product, Message, Threads, Post };
+module.exports = { User, Product, Message, Thread, Post };
